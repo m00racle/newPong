78f9d160f683c8303d67cfd5862e43fff0506741
mooracle <yanuar.prakosa@gmail.com>
2021-06-12 13:11:26 +0700

    add pad class and its specs

---
+++ pad.lua
@@ -0,0 +1,13 @@
+-- this is the file specific to model players' pads.
+
+-- pads 
+-- size, width and length
+
+-- methods:
+-- pad:create(length, width, posX, posY, mode):
+
+
+-- pad:reachedEdge(topEdge, bottomEdge):
+
+
+-- pad.move(deltaX, deltaY):
